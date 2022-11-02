14644da49f53ea85108b12eca1ebed2bdf8ed500
PantosThn <pantos.thn@gmail.com>
2022-11-02 18:01:43 +0200

    added jupyter notebooks

--- app/crud.py
+++ app/crud.py
@@ -1,9 +1,19 @@
+import uuid
 from cassandra.cqlengine.management import sync_table
 from .db import get_session
-from .models import Product
+from .models import Product, ProductScrapeEvent
 
 session = get_session()
 sync_table(Product)
 
 def create_entry(data:dict):
     return Product.create(**data)
+
+def create_scrape_entry(data:dict):
+    data['uuid'] = uuid.uuid1() # includes a timestamp
+    return ProductScrapeEvent.create(**data)
+
+def add_scrape_event(data:dict):
+    product = create_entry(data)
+    scrape_ojb = create_scrape_entry(data)
+    return product, scrape_ojb
--- app/models.py
+++ app/models.py
@@ -10,3 +10,10 @@ class Product(Model): # -> table
     __keyspace__ = "scrapper_app" #
     asin = columns.Text(primary_key=True, required=True)
     title = columns.Text()
+
+class ProductScrapeEvent(Model):
+    __keyspace__ = "scrapper_app"
+    uuid = columns.UUID(primary_key=True)
+    asin = columns.Text(index=True)
+    title = columns.Text()
+    price_str = columns.Text(default="-100")
