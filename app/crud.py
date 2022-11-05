e1b29513982746608592dad9b53f18f70ff524b6
PantosThn <pantos.thn@gmail.com>
2022-11-05 11:42:15 +0200

    added config files and main

--- app/crud.py
+++ app/crud.py
@@ -5,6 +5,7 @@ from .models import Product, ProductScrapeEvent
 
 session = get_session()
 sync_table(Product)
+sync_table(ProductScrapeEvent)
 
 def create_entry(data:dict):
     return Product.create(**data)
@@ -13,7 +14,7 @@ def create_scrape_entry(data:dict):
     data['uuid'] = uuid.uuid1() # includes a timestamp
     return ProductScrapeEvent.create(**data)
 
-def add_scrape_event(data:dict):
+def add_scrape_event(data:dict, fresh=False):
     product = create_entry(data)
     scrape_ojb = create_scrape_entry(data)
     return product, scrape_ojb
