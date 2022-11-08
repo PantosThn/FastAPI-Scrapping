c24b848c328567a387c1f2828358f798dda38db9
PantosThn <pantos.thn@gmail.com>
2022-11-08 16:35:22 +0200

    added utils file

--- app/schema.py
+++ app/schema.py
@@ -2,12 +2,23 @@ from uuid import UUID
 from typing import Optional
 from pydantic import BaseModel
 
+class ProductListSchema(BaseModel):
+    asin: str
+    title: Optional[str]
+    price_str: Optional[str]
+
 class ProductSchema(BaseModel):
     asin: str
     title: Optional[str]
-
+    price_str: Optional[str]
 
 class ProductScrapeEventSchema(BaseModel):
     uuid: UUID
     asin: str
     title: Optional[str]
+    price_str: Optional[str]
+
+class ProductScrapeEventDetailSchema(BaseModel):
+    asin: str
+    title: Optional[str]
+    price_str: Optional[str]
